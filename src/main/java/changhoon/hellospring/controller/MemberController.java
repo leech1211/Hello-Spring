1825e64dde5105ecadae3b9abc1930d56f5f20f8
leech1211 <leech1211@nate.com>
2022-06-22 20:20:20 +0900

    16 회원웹기능 등록

--- src/main/java/changhoon/hellospring/controller/MemberController.java
+++ src/main/java/changhoon/hellospring/controller/MemberController.java
@@ -1,9 +1,12 @@
 package changhoon.hellospring.controller;
 
+import changhoon.hellospring.domain.Member;
 import changhoon.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -14,5 +17,21 @@ public class MemberController {
     public MemberController(MemberService memberService)
     {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm()
+    {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form)
+    {
+         Member member = new Member();
+         member.setName(form.getName());
+
+         memberService.join(member);
+         return "redirect:/";   //회원가입이 끝났으므로 홈화면으로 보낸다
     }
 }
